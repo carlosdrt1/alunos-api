674befbe40f1e22189e6d71fa37b9d29f3cf19ea
carlosdrt1 <carlosduarteinfo@gmail.com>
2025-05-03 21:41:23 -0300

    crud alunos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -2,8 +2,13 @@ import Aluno from "../models/Aluno.js";
 
 class AlunoController {
   async index(req, res) {
-    const alunos = await Aluno.findAll();
-    res.json(alunos);
+    try {
+      const alunos = await Aluno.findAll();
+      res.json(alunos);
+    } catch (error) {
+      console.log(error);
+      res.json(null);
+    }
   }
 
   async create(req, res) {
@@ -14,6 +19,51 @@ class AlunoController {
       return res.status(400).json({
         errors: error.errors.map((e) => e.message),
       });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const aluno = await Aluno.findByPk(req.params.id);
+
+      if (!aluno) {
+        return res.status(404).json({ message: "Aluno não encontrado" });
+      }
+
+      return res.json(aluno);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const aluno = await Aluno.findByPk(req.params.id);
+
+      if (!aluno) {
+        return res.status(404).json({ message: "Aluno não encontrado" });
+      }
+
+      const newAluno = await aluno.update(req.body);
+      res.json(newAluno);
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((e) => e.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const aluno = await Aluno.findByPk(req.params.id);
+      if (!aluno) {
+        return res.status(404).json({ message: "Aluno não encontrado" });
+      }
+
+      await aluno.destroy(req.params.id);
+      return res.json({ message: "Aluno deletado com sucesso" });
+    } catch (error) {
+      console.log(error);
     }
   }
 }
--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,8 +1,12 @@
+import loginRequired from "../middlewares/loginRequired.js";
+import alunoController from "../controllers/AlunoController.js";
 import express from "express";
 const router = express.Router();
-import alunoController from "../controllers/AlunoController.js";
 
 router.get("/", alunoController.index);
-router.post("/", alunoController.create);
+router.post("/", loginRequired, alunoController.create);
+router.get("/{:id}", alunoController.show);
+router.put("/{:id}", loginRequired, alunoController.update);
+router.delete("/{:id}", loginRequired, alunoController.delete);
 
 export default router;
